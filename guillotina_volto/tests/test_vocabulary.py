3b72592632b383f61f353c1b34f06b6f2935b73d
nilbacardit26 <n.bacardit@iskra.cat>
2025-10-16 16:40:17 +0200

    passing tests

--- guillotina_volto/tests/test_vocabulary.py
+++ guillotina_volto/tests/test_vocabulary.py
@@ -7,12 +7,16 @@ pytestmark = pytest.mark.asyncio
 
 @pytest.mark.skipif(NOT_POSTGRES, reason="Only PG")
 async def test_vocabulary(cms_requester):
-    resp, status = await cms_requester("GET", "/db/guillotina/@vocabularies/content_types")
+    resp, status = await cms_requester(
+        "GET", "/db/guillotina/@vocabularies/content_types"
+    )
     assert status == 200
     assert "Page" in list(map(lambda x: x["title"], resp["items"]))
-    assert resp["items_total"] == 9
+    assert resp["items_total"] == 8
 
-    resp, status = await cms_requester("GET", "/db/guillotina/@vocabularies/workflow_states")
+    resp, status = await cms_requester(
+        "GET", "/db/guillotina/@vocabularies/workflow_states"
+    )
     assert status == 200
     assert "Private" in list(map(lambda x: x["title"], resp["items"]))
     assert resp["items_total"] == 2
